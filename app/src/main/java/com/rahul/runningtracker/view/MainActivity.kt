e65ad84c67442161144f50b564a2dafc1182d0f7
rahulcravi9766 <87615703+rahulcravi9766@users.noreply.github.com>
2022-01-24 16:34:02 +0530

    UI creation

--- app/src/main/java/com/rahul/runningtracker/view/MainActivity.kt
+++ app/src/main/java/com/rahul/runningtracker/view/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.rahul.runningtracker.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.rahul.runningtracker.R
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** adding custom toolbar ****/
         setSupportActionBar(toolbar)
+
+        /** setting bottom bar with the activity *****/
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runningFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/com/rahul/runningtracker/view/fragments/SetUpFragment.kt
+++ app/src/main/java/com/rahul/runningtracker/view/fragments/SetUpFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.rahul.runningtracker.R
 import com.rahul.runningtracker.databinding.FragmentSetUpBinding
+import kotlinx.android.synthetic.main.fragment_set_up.*
 
 
 class SetUpFragment : Fragment() {
@@ -19,5 +22,13 @@ class SetUpFragment : Fragment() {
         binding = FragmentSetUpBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+          tvContinue.setOnClickListener {
+              findNavController().navigate(R.id.action_setUpFragment_to_runningFragment)
+          }
     }
 }
